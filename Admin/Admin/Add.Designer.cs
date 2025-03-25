423b05a8406136a7af4f17efed9cb71902db6e66
boosoby <bo.osoby@gmail.com>
2025-03-25 18:17:53 +0700

    Добавил наброски форм добавления и изменения товара

---
+++ Admin/Admin/Add.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Admin
+{
+    partial class Add
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox3 = new TextBox();
+            label2 = new Label();
+            dataGridView1 = new DataGridView();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(262, 396);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Наличие размера (шт.)";
+            textBox3.Size = new Size(240, 23);
+            textBox3.TabIndex = 16;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(262, 251);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 15);
+            label2.TabIndex = 15;
+            label2.Text = "Размеры";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(262, 269);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(240, 118);
+            dataGridView1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(262, 131);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Описание";
+            textBox2.Size = new Size(240, 117);
+            textBox2.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(262, 102);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Название товара";
+            textBox1.Size = new Size(240, 23);
+            textBox1.TabIndex = 12;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(262, 53);
+            button1.Name = "button1";
+            button1.Size = new Size(172, 43);
+            button1.TabIndex = 11;
+            button1.Text = "Изменить картинку";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 53);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(244, 366);
+            pictureBox1.TabIndex = 10;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(233, 32);
+            label1.TabIndex = 9;
+            label1.Text = "Добавление товара";
+            // 
+            // Add
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox3);
+            Controls.Add(label2);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "Add";
+            Text = "Add";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox3;
+        private Label label2;
+        private DataGridView dataGridView1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Label label1;
+    }
+}
---
+++ Admin/Admin/Edit.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Admin
+{
+    partial class Edit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            dataGridView1 = new DataGridView();
+            label2 = new Label();
+            textBox3 = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(223, 32);
+            label1.TabIndex = 1;
+            label1.Text = "Изменение товара";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 53);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(244, 366);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(262, 53);
+            button1.Name = "button1";
+            button1.Size = new Size(172, 43);
+            button1.TabIndex = 3;
+            button1.Text = "Изменить картинку";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(262, 102);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Название товара";
+            textBox1.Size = new Size(240, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(262, 131);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Описание";
+            textBox2.Size = new Size(240, 117);
+            textBox2.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(262, 269);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(240, 118);
+            dataGridView1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(262, 251);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 15);
+            label2.TabIndex = 7;
+            label2.Text = "Размеры";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(262, 396);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Наличие размера (шт.)";
+            textBox3.Size = new Size(240, 23);
+            textBox3.TabIndex = 8;
+            // 
+            // Edit
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox3);
+            Controls.Add(label2);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "Edit";
+            Text = "Edit";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private DataGridView dataGridView1;
+        private Label label2;
+        private TextBox textBox3;
+    }
+}
